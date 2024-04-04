c9e8f120c3b9ae9f1c3850a17a7e92b9872597d7
Israel Ogunsola <110337578+Issybobo@users.noreply.github.com>
2024-04-04 02:36:15 +0100

    added invite others

--- src/components/organizationScreen/InviteOthers.jsx
+++ src/components/organizationScreen/InviteOthers.jsx
@@ -8,18 +8,26 @@ const InviteOthers = () => {
   return (
     <div className=' flex '>
       <div className='w-1/2 bg-white flex flex-col justify-start items-start '>
-        <div className="pl-4 pt-4 flex flex-row justify-between">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+        <div className="pl-4 pt-4 flex flex-row justify-center">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-9 h-9">
     <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
    
  </svg> 
- <h1 className='text-bold-3xl'>Organization</h1>
- <div class="flex space-x-4 text-blue-500 ..."></div>
+ <h1 className='text-2xl font-bold text-center' >Organization</h1>
 
 
         {/** <img src={logopic} alt="accounts goal image" className='w-64 h-64 text-blue-500' />
         * */} 
         </div>
+       {/**  <div className='flex flex-row justify-start'>
+        <ul className="steps">
+  <li className="step step-primary"></li>
+  <li className="step step-primary"></li>
+  <li className="step"></li>
+  <li className="step"></li>
+</ul>
+        </div> * */}
+      
         <br/><br/>
         <div className="text-right px-32">
           <h1 className='text-3xl text-bold text-left'>Invite Others</h1>
